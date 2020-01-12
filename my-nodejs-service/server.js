c37fbf3770e10a15685ae2b04d9f27306162a426
stevenlai41 <steven_lai4@hotmail.com>
2020-01-12 03:05:48 -0800

    json works

--- my-nodejs-service/server.js
+++ my-nodejs-service/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path')
 var uuid = require('node-uuid');
+var fs = require('fs');
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended:false}))
@@ -14,9 +15,36 @@ app.get('/', (req, res) => {
 });
 
 app.post('/create', (req, res) => {
+
   var randomID = uuid.v4();
   console.log(randomID);
   res.render('link');
+
+  let name = req.body.name;
+  let event = req.body.event;
+  let date = req.body.date;
+ 	let location = req.body.location;
+
+ 	var obj = {};
+
+ 	fs.readFile('meetwhere.json', 'utf8', function(err, data){
+ 		if(err){
+ 			console.log(err);
+ 		}
+ 		
+ 		if(data == ""){
+ 			let array = [{"time": date},{name:location}];
+ 			obj[event] = array; 			
+ 			fs.writeFileSync('meetwhere.json', JSON.stringify(obj));
+ 		}
+ 		else{
+ 			let array = [{"time": date},{name:location}];
+ 			obj = JSON.parse(data);
+ 			obj[event] = array;
+
+ 			fs.writeFileSync('meetwhere.json', JSON.stringify(obj));
+ 		}
+ 	});
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
